8a03cd6894bf8ad9dcfd0ed73ecd22f5c1d8050f
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-03-15 21:18:16 +0530

    learning

---
+++ Day02/index.js
@@ -0,0 +1,83 @@
+// console.log("hello World!");
+
+/*Data Types */
+
+
+//////////////////
+/*
+// Number
+var age = 23;
+console.log(age);
+
+// String
+const firstName = "Nitesh";
+console.log(firstName);
+
+// Boolean
+const ageCalc = age > 20 ? "Print Yes" : "Print No";
+console.log(ageCalc);
+
+// Undefined
+var x;
+console.log(x);
+
+// Null
+const xyz=null;
+console.log(null);
+
+// Symbol
+var x=Symbol(2);
+var y= Symbol(2)
+let results=x===y?"true":"false"
+console.log(results)
+
+// BigInt
+const num= BigInt(1214235345346563543255);
+console.log(num);
+*/
+
+/* VAR , LET , CONST*/
+
+// var x=23;
+// {
+// var x =43;
+// }
+// console.log(x);
+// That means var is a global scope.
+
+// let x=23;
+// {
+//     let x=234;
+//     console.log(x);
+// }
+// console.log(x);
+
+// const x=23;
+// x=233;
+// console.log(x);
+
+////////////////////////////////////
+// Basic Operators
+
+// Assignment operators
+
+// let x=10+5;
+// x*=10;
+// ++x;
+// console.log(x);
+
+////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
