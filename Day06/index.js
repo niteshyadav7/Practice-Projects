77ae74b5be6f153be5234b6c91da9dc32ad269cc
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-03-21 05:49:04 +0530

    learning

---
+++ Day06/index.js
@@ -0,0 +1,16 @@
+'use strict';
+// const arr =[2,3,4,5,6]
+
+// const newArr =arr.map(mov=>2*mov);
+// console.log(newArr);
+
+// STRINGS:
+
+
+const airline ="TAP Air INDIA";
+const plane='A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(plane[3]);
