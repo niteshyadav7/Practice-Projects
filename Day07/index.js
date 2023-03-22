96a80324e34562896128c8fe7948a0457d4623bf
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-03-22 11:11:40 +0530

    learning

--- Day07/index.js
+++ Day07/index.js
@@ -1,1 +1,43 @@
 'use strict';
+
+// Default Parameters:
+
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numberOfPassenger = 1,
+  price = 199 * numberOfPassenger
+) {
+  const booking = {
+    flightNum,
+    numberOfPassenger,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('18ME#2', 12, undefined);
+// createBooking('18ME#1', undefined, undefined);
+*/
+
+//////////////////////////////////////
+
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+// lufthansa.book(239, "Nitesh");
+// lufthansa.book(240, "Golu");
+// console.log(lufthansa.bookings);
+
+
