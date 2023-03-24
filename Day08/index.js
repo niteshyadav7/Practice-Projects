54ba89b2d696ce6ffc2f6c8efa64db92617443da
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-03-24 12:02:59 +0530

    learning

--- Day08/index.js
+++ Day08/index.js
@@ -11,26 +11,48 @@ const rendered = function (first) {
   click.insertAdjacentHTML("beforeend", html);
 };
 
-const apiCall = function (country) {
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const apiCall = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener("load", function () {
-    const [first] = JSON.parse(this.responseText);
-    console.log(first);
-    rendered(first);
-    const [nei] = first.borders[0];
-    // rendered(nei)
-    const request2 = new XMLHttpRequest();
-    request2.open("GET", `https://restcountries.com/v3.1/name/${nei}`);
-    request2.send();
+//   request.addEventListener("load", function () {
+//     const [first] = JSON.parse(this.responseText);
+//     console.log(first);
+//     rendered(first);
+//     const [nei] = first.borders[0];
+//     // rendered(nei)
+//     const request2 = new XMLHttpRequest();
+//     request2.open("GET", `https://restcountries.com/v3.1/name/${nei}`);
+//     request2.send();
 
-    request2.addEventListener("load", function () {
-      const [data2] = JSON.parse(this.responseText);
-      console.log(data2);
-      rendered(data2);
-    });
-  });
-};
-apiCall("china");
+//     request2.addEventListener("load", function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+//       rendered(data2);
+//     });
+//   });
+// };
+// apiCall("china");
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response1) {
+//       return response1.json();
+//     })
+//     .then(function (response2) {
+//       console.log(response2[0].flags.svg);
+//       // console.log(response2[0].flags.png);
+//       rendered(response2[0]);
+//     });
+// };
+// getCountryData("pakistan");
+
+
+
+const whereAmI =async function(country){
+ const res=await fetch (`https://restcountries.com/v3.1/name/${country}`)
+ const data=await res.json();
+ console.log(data);
+}
+whereAmI('finland')
