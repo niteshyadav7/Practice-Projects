1acdbd94325fafd98dc2488b15e17b675c753606
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-03-24 14:20:22 +0530

    learning

--- React_Coding_Ninjas/Chapter02/index.js
+++ React_Coding_Ninjas/Chapter02/index.js
@@ -1,10 +1,27 @@
-const jsx = (
+// const jsx = (
+//   <>
+//     <h1>Hello React</h1>
+//     <div>We are learning React by Coding Ninjas</div>
+//   </>
+// );
+
+// const el = document.getElementById("root");
+// const root = ReactDOM.createRoot(el);
+// root.render(jsx);
+
+const jsxReact = (
   <>
-    <h1>Hello React</h1>
-    <div>We are learning React by Coding Ninjas</div>
+    <h1>About React</h1>
+    <h3>React is the javaScript library.</h3>
+    <ul>
+      <li>React is mainly used to make Single Page Applications.</li>
+      <li>React is Composable in Nature.</li>
+      <li>React is Declartive in Nature.</li>
+      <li>React uses the virtual DOM</li>
+    </ul>
   </>
 );
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
-root.render(jsx);
+root.render(jsxReact);
