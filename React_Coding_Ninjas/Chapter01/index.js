19aec706827cd3601d7bf5b6f17cf7ef03cedb50
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-03-24 11:46:25 +0530

    learning

---
+++ React_Coding_Ninjas/Chapter01/index.js
@@ -0,0 +1,19 @@
+/*
+const heading =React.createElement('img',{src:"./logo192.png"})
+const el =document.getElementById('root')
+const root=ReactDOM.createRoot(el)
+
+root.render(heading )
+*/
+
+const jsx = (
+  <>
+    <h1>Hello world!</h1>
+    <p>This is created by Me</p>
+  </>
+);
+
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+root.render(jsx);
